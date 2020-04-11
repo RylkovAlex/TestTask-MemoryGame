748c2deb3b03b0874bc75a571c8998ee97aa6a75
RylkovAlex <aleksandr_rylkov@epam.com>
2020-04-11 19:41:07 +0300

    Updates

---
+++ precache-manifest.292418638210316dec5c4e01ac451d8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277199b133ab9a1a0e9f235c8aa61e28",
+    "url": "/CDG-memory-game/index.html"
+  },
+  {
+    "revision": "ce2e2c2abb0c5025a8aa",
+    "url": "/CDG-memory-game/static/css/main.4fa314cf.chunk.css"
+  },
+  {
+    "revision": "972f014cea3d540f7de8",
+    "url": "/CDG-memory-game/static/js/2.3bc75b19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CDG-memory-game/static/js/2.3bc75b19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce2e2c2abb0c5025a8aa",
+    "url": "/CDG-memory-game/static/js/main.0f166b33.chunk.js"
+  },
+  {
+    "revision": "bcc9c6a8af80d92ae980",
+    "url": "/CDG-memory-game/static/js/runtime-main.007ba9b6.js"
+  }
+]);
